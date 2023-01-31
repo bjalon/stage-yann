91d8dfca02c192a83a186350b57a6bdb04b7ffb1
Benjamin JALON <bjalon@qastia.com>
2023-01-31 15:59:24 +0100

    Add audio on fail and success

--- js/index.js
+++ js/index.js
@@ -4,6 +4,9 @@ document.getElementById("a").innerHTML = a
 document.getElementById("b").innerHTML = b
 var resultatSaisi = null
 
+var successfulAudio = new Audio('audio/win.wav');
+var failedAudio = new Audio('audio/fail.wav');
+
 function verifierResultatAddition() {
     resultatSaisi = parseInt(document.getElementById("inputResultAddition").value)
     var resultatAttendu = a + b
@@ -12,9 +15,11 @@ function verifierResultatAddition() {
 // alert("Le résultat saisi est bon")
         document.getElementById("informationAddition").innerHTML = "Le résultat est correct"
         document.getElementById("informationAddition").setAttribute("style", "color: lightgreen")
+        successfulAudio.play();
     } else {
         document.getElementById("informationAddition").innerHTML = "Le résultat est faux"
         document.getElementById("informationAddition").setAttribute("style", "color: red")
+        failedAudio.play();
 // alert("Le résultat saisi est faux")
     }
 
@@ -27,9 +32,11 @@ function verifierResultatMultiplication() {
     if (isCorrect) {
         document.getElementById("informationMultiplication").innerHTML = "Tu es un génie !"
         document.getElementById("informationMultiplication").setAttribute("style", "color: lightgreen")
+        successfulAudio.play();
     } else {
-        document.getElementById("informationMultiplication").innerHTML ="Tu t'es trompé ☻"
+        document.getElementById("informationMultiplication").innerHTML = "Tu t'es trompé ☻"
         document.getElementById("informationMultiplication").setAttribute("style", "color: red")
+        failedAudio.play();
     }
 
 }
