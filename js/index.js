d91aa959f29d1447a4ea7eb4fca5b7510bb3ad7e
Benjamin JALON <bjalon@qastia.com>
2023-01-31 15:18:07 +0100

    init

---
+++ js/index.js
@@ -0,0 +1,35 @@
+var a = 12
+var b = 13
+document.getElementById("a").innerHTML = a
+document.getElementById("b").innerHTML = b
+var resultatSaisi = null
+
+function verifierResultatAddition() {
+    resultatSaisi = parseInt(document.getElementById("inputResultAddition").value)
+    var resultatAttendu = a + b
+    var isCorrect = (resultatAttendu === resultatSaisi)
+    if (isCorrect) {
+// alert("Le résultat saisi est bon")
+        document.getElementById("information").innerHTML = "Le résultat est correct"
+        document.getElementById("information").setAttribute("style", "color: lightgreen")
+    } else {
+        document.getElementById("information").innerHTML = "Le résultat est faux"
+        document.getElementById("information").setAttribute("style", "color: red")
+// alert("Le résultat saisi est faux")
+    }
+
+}
+
+function verifierResultatMultiplication() {
+    resultatSaisi = parseInt(document.getElementById("inputResultMultiplication").value)
+    var resultatAttendu = a * b
+    var isCorrect = resultatAttendu === resultatSaisi
+    if (isCorrect) {
+        document.getElementById("informationMultiplication").innerHTML = "Tu es un génie !"
+        document.getElementById("informationMultiplication").setAttribute("style", "color: lightgreen")
+    } else {
+        document.getElementById("informationMultiplication").innerHTML ="Tu t'es trompé ☻"
+        document.getElementById("informationMultiplication").setAttribute("style", "color: red")
+    }
+
+}
